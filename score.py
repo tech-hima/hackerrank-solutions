bcb5089ce05a32bfc1cc86982e8d9089e2f8eb10
Himanshi Goyal <92114186+tech-hima@users.noreply.github.com>
2021-11-04 16:23:27 +0530

    Create score.py

---
+++ score.py
@@ -0,0 +1,8 @@
+# Find the Runner-Up Score! in Python 
+if __name__ == '__main__':- Hacker Rank Solution
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    # Find the Runner-Up Score! in Python - 
+    print(sorted(list(set(arr)))[-2])
+    
